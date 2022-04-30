b9a759e38bc44e60adeee3851410049ef3f4416a
togo-mentor <uint2435@gmail.com>
2022-04-30 09:24:53 +0900

    amplify setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifytest56d3718256d37182": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
